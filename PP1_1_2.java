c33399c7af74780039cb9a6327bc6b31a9bb0675
AnujModi13 <98648535+AnujModi13@users.noreply.github.com>
2022-07-30 15:14:37 +0530

    Update PP1_1_2.java

--- PP1_1_2.java
+++ PP1_1_2.java
@@ -8,19 +8,25 @@ startOz("bzoo") → "z"
 startOz("oxx") → "o"*/
 
 import java.util.Scanner;
+public class Main
+{
+  public static void main (String[]args)
+  {
+    Scanner sc = new Scanner (System.in);
+      System.out.println ("Enter String:");
+    String A = sc.nextLine ();
+      startOZ (A);
+  }
+  static void startOZ (String a)
+  {
+    if (a.charAt (0) == 'O' || a.charAt (0) == 'o')
+      {
+	System.out.print (a.charAt (0));
+      }
+    if (a.charAt (1) == 'z' || a.charAt (1) == 'Z')
+      {
+	System.out.print (a.charAt (1));
+      }
+  }
 
-public class PP1_1_2 {
-    public static void main(String[] args) {
-      Scanner sc=new Scanner(System.in);
-        String a=sc.nextLine();
-        if(a.charAt(0)=='o')
-        {
-            System.out.println('o');
-        }
-        if(a.charAt(1)=='z')
-        {
-            System.out.println('z');
-        }
-
-    }
 }
