7626c11de6f8c27ac4202f83732d63841a71e41e
AnujModi13 <98648535+AnujModi13@users.noreply.github.com>
2022-07-30 15:12:29 +0530

    Update PP1_1_2.java

--- PP1_1_2.java
+++ PP1_1_2.java
@@ -1,3 +1,12 @@
+// 21CE072 _Anuj Modi
+
+/*Given a string, return a string made of the first 2 chars (if present), however include 
+first char only if it is 'o' and include the second only if it is 'z', so "ozymandias" 
+yields "oz".
+startOz("ozymandias") → "oz"
+startOz("bzoo") → "z"
+startOz("oxx") → "o"*/
+
 import java.util.Scanner;
 
 public class PP1_1_2 {
