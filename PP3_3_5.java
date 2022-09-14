2a485cc3520d9171d81e0336d104b9411a504b25
AnujModi13 <98648535+AnujModi13@users.noreply.github.com>
2022-09-15 01:19:31 +0530

    Add files via upload

---
+++ PP3_3_5.java
@@ -0,0 +1,13 @@
+import mypackage.Class;
+public class PP3_3_5 {
+    public static void main(String[] args) {
+        
+        int id=21072;
+        String name="Anuj";
+        Class c= new Class();
+        c.setId(id);
+        c.setName(name);
+        c.getId();
+        c.getName();
+    }
+}
---
+++ PP3_3_6.java
@@ -0,0 +1,10 @@
+
+import mypack.Mypac;
+public class PP3_3_6 {
+    public static void main(String[] args) {
+        Mypac m1=new Mypac();
+        String st="11011";
+        m1.peli(st);
+
+    }
+}
